fcc6f3143955193ce5d838b6c8ad84bf9cf9aec9
timmens <mensingertim@gmail.com>
2025-12-29 19:34:15 +0100

    Fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,19 @@
-import optimagic.optimizers.tranquilo as t
 from _pytest.monkeypatch import MonkeyPatch
 
-_mp = MonkeyPatch()
+from tranquilo.config import IS_OPTIMAGIC_INSTALLED
 
+if IS_OPTIMAGIC_INSTALLED:
+    import optimagic.optimizers.tranquilo as t
 
-def pytest_configure(config):
-    _mp.setattr(
-        t, "IS_TRANQUILO_VERSION_NEWER_OR_EQUAL_TO_0_1_0", "patched-value", raising=True
-    )
+    _mp = MonkeyPatch()
 
+    def pytest_configure(config):
+        _mp.setattr(
+            t,
+            "IS_TRANQUILO_VERSION_NEWER_OR_EQUAL_TO_0_1_0",
+            "patched-value",
+            raising=True,
+        )
 
-def pytest_unconfigure(config):
-    _mp.undo()
+    def pytest_unconfigure(config):
+        _mp.undo()
